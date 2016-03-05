a8deea72ea5a352a3f7d6e7aa8adaf9dc0a88368
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2016-03-05 10:45:10 +0100

    Rename client to echo

--- cmd/echo.go
+++ cmd/echo.go
@@ -10,27 +10,33 @@ import (
 )
 
 const (
-	address     = "localhost:65432"
-	defaultName = "world"
+	grpcEndpoint = "localhost:65432"
+	restEndpoint = "http://localhost:8080"
+	defaultName  = "world"
 )
 
 // clientCmd represents the client command
-var clientCmd = &cobra.Command{
-	Use:   "client",
+var echoCmd = &cobra.Command{
+	Use:   "echo",
 	Short: "A brief description of your command",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		client := c.GrpcClient{}
-		client.Init(address)
+		client := c.NewGrpcClient(grpcEndpoint)
 		name := defaultName
 		r, err := client.LogIn(&pb.LogInRequest{Username: name, Password: "secret"})
 		if err != nil {
 			log.Fatalf("could not log in: %v", err)
 		}
 		log.Printf("Logged in: %s", r)
+		v, err := client.Validate(&pb.CredentialsRequest{Token: r.Token})
+		if err != nil {
+			log.Fatalf("could not validate: %v", err)
+		}
+		log.Printf("Validated: %s", v)
+
 	},
 }
 
 func init() {
-	RootCmd.AddCommand(clientCmd)
+	RootCmd.AddCommand(echoCmd)
 }
