7e0f39db886d675e943bdd47505b3d3953f9fd6f
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2016-03-05 10:45:41 +0100

    Implement REST client, make interface more idiomatic

--- client/client.go
+++ client/client.go
@@ -1,7 +1,11 @@
 package client
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 
 	ctx "golang.org/x/net/context"
 
@@ -11,8 +15,6 @@ import (
 
 // AuthClient abstracts grpc and rest clients
 type AuthClient interface {
-	// Init initializes client with given endpoint
-	Init(endpoint string)
 	// LogIn logs in
 	LogIn(r *pb.LogInRequest) (*pb.AuthResponse, error)
 	// Validate validates
@@ -24,21 +26,67 @@ type GrpcClient struct {
 	client pb.AuthenticatorClient
 }
 
-// Init initializes client with given endpoint
-func (g *GrpcClient) Init(endpoint string) {
+// NewGrpcClient creates a new grpc client for given endpoint
+func NewGrpcClient(endpoint string) AuthClient {
 	conn, err := grpc.Dial(endpoint, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	g.client = pb.NewAuthenticatorClient(conn)
+	return GrpcClient{pb.NewAuthenticatorClient(conn)}
 }
 
 // LogIn logs in
-func (g *GrpcClient) LogIn(r *pb.LogInRequest) (*pb.AuthResponse, error) {
+func (g GrpcClient) LogIn(r *pb.LogInRequest) (*pb.AuthResponse, error) {
 	return g.client.LogIn(ctx.Background(), r)
 }
 
 // Validate validates
-func (g *GrpcClient) Validate(r *pb.CredentialsRequest) (*pb.AuthResponse, error) {
+func (g GrpcClient) Validate(r *pb.CredentialsRequest) (*pb.AuthResponse, error) {
 	return g.client.Validate(ctx.Background(), r)
+}
+
+// RestClient is a REST implementation of AuthClient
+type RestClient struct {
+	endpoint string
+}
+
+// NewRestClient creates a new rest client for given endpoint
+func NewRestClient(endpoint string) AuthClient {
+	return RestClient{endpoint: endpoint}
+}
+
+// LogIn logs in
+func (c RestClient) LogIn(r *pb.LogInRequest) (*pb.AuthResponse, error) {
+	req, err := json.Marshal(r)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := http.Post(c.endpoint+"/v1/login", "application/json", bytes.NewReader(req))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var ar pb.AuthResponse
+	json.NewDecoder(resp.Body).Decode(&ar)
+	fmt.Println(resp.Body)
+	return &ar, nil
+}
+
+// Validate validates
+func (c RestClient) Validate(r *pb.CredentialsRequest) (*pb.AuthResponse, error) {
+	req, err := json.Marshal(r)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := http.Post(c.endpoint+"/v1/validate", "application/json", bytes.NewReader(req))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var ar pb.AuthResponse
+	err = json.NewDecoder(resp.Body).Decode(&ar)
+	if err != nil {
+		return nil, err
+	}
+	return &ar, nil
 }
