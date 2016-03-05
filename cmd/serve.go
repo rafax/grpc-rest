a9966a398de960a186c02eea5f27966b27eb644d
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2016-03-05 12:25:49 +0100

    Move server to it's own package

--- cmd/serve.go
+++ cmd/serve.go
@@ -3,31 +3,17 @@ package cmd
 import (
 	"fmt"
 	"log"
-	"math/rand"
 	"net"
-	"strconv"
-	"time"
-
-	"golang.org/x/net/context"
 
 	"google.golang.org/grpc"
 
 	pb "github.com/rafax/grpc-rest/pb"
+	server "github.com/rafax/grpc-rest/server"
+
 	"github.com/spf13/cobra"
 )
 
 const port = ":65432"
-
-type server struct{}
-
-func (s *server) LogIn(c context.Context, lir *pb.LogInRequest) (*pb.AuthResponse, error) {
-	//fmt.Println("Logged in ", lir)
-	return &pb.AuthResponse{Token: strconv.Itoa(rand.Int()), ValidUntil: time.Now().String()}, nil
-}
-
-func (s *server) Validate(ctx context.Context, in *pb.CredentialsRequest) (*pb.AuthResponse, error) {
-	return &pb.AuthResponse{Token: strconv.Itoa(rand.Int()), ValidUntil: time.Now().String()}, nil
-}
 
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
@@ -40,13 +26,12 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-
 		lis, err := net.Listen("tcp", port)
 		if err != nil {
 			log.Fatalf("failed to listen: %v", err)
 		}
 		s := grpc.NewServer()
-		pb.RegisterAuthenticatorServer(s, &server{})
+		pb.RegisterAuthenticatorServer(s, server.NewAuthServer())
 		fmt.Println("Running on ", port)
 		s.Serve(lis)
 	},
---
+++ server/server.go
@@ -0,0 +1,53 @@
+package server
+
+import (
+	"errors"
+	"math/rand"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/rafax/grpc-rest/pb"
+	"golang.org/x/net/context"
+)
+
+// AuthServer stores tokens issued for users
+type AuthServer struct {
+	tokensMutex *sync.RWMutex
+	tokens      map[string]time.Time
+}
+
+// NewAuthServer creates a new AuthServer
+func NewAuthServer() AuthServer {
+	var tm sync.RWMutex
+	return AuthServer{tokens: make(map[string]time.Time), tokensMutex: &tm}
+}
+
+// LogIn logs issues a token valid for 24 hours
+func (s AuthServer) LogIn(c context.Context, lir *pb.LogInRequest) (*pb.AuthResponse, error) {
+	t := strconv.Itoa(rand.Int())
+	validUntil := time.Now().Add(time.Duration(24) * time.Hour)
+	s.tokensMutex.Lock()
+	s.tokens[t] = validUntil
+	s.tokensMutex.Unlock()
+	time.Sleep(time.Duration(10) * time.Millisecond)
+	return &pb.AuthResponse{Token: t, ValidUntil: validUntil.String()}, nil
+}
+
+// Validate checks if token is valid and was issued by this server
+func (s AuthServer) Validate(ctx context.Context, in *pb.CredentialsRequest) (*pb.AuthResponse, error) {
+	s.tokensMutex.RLock()
+	validUntil, ok := s.tokens[in.Token]
+	s.tokensMutex.RUnlock()
+	time.Sleep(time.Duration(10) * time.Millisecond)
+	if ok {
+		if time.Now().After(validUntil) {
+			s.tokensMutex.Lock()
+			delete(s.tokens, in.Token)
+			s.tokensMutex.Unlock()
+			return nil, errors.New("Token expired")
+		}
+		return &pb.AuthResponse{Token: in.Token, ValidUntil: validUntil.String()}, nil
+	}
+	return nil, errors.New("Invalid token")
+}
