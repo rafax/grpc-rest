ff99a5eed627069058d7b83aa8b06cc6150ad535
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2016-03-05 10:45:59 +0100

    Enable both REST endpoints

--- pb/auth.pb.gw.go
+++ pb/auth.pb.gw.go
@@ -42,6 +42,19 @@ func request_Authenticator_LogIn_0(ctx context.Context, client AuthenticatorClie
 
 }
 
+func request_Authenticator_Validate_0(ctx context.Context, client AuthenticatorClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CredentialsRequest
+	var metadata runtime.ServerMetadata
+
+	if err := json.NewDecoder(req.Body).Decode(&protoReq); err != nil {
+		return nil, metadata, grpc.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.Validate(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterAuthenticatorHandlerFromEndpoint is same as RegisterAuthenticatorHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterAuthenticatorHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -95,13 +108,40 @@ func RegisterAuthenticatorHandler(ctx context.Context, mux *runtime.ServeMux, co
 
 	})
 
+	mux.Handle("POST", pattern_Authenticator_Validate_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		resp, md, err := request_Authenticator_Validate_0(runtime.AnnotateContext(ctx, req), client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, w, req, err)
+			return
+		}
+
+		forward_Authenticator_Validate_0(ctx, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
 var (
 	pattern_Authenticator_LogIn_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "login"}, ""))
+
+	pattern_Authenticator_Validate_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "validate"}, ""))
 )
 
 var (
 	forward_Authenticator_LogIn_0 = runtime.ForwardResponseMessage
+
+	forward_Authenticator_Validate_0 = runtime.ForwardResponseMessage
 )
